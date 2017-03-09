ab0276aff46f6ecf7deaad2c9ec60aa70c182401
nicolasghirello <nicolas.rosario@hotmail.com>
2017-03-09 14:25:08 -0300

    estrutura, volume.cpp, volume.h, main.cpp(incompleto)

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+while
+cout(1) Triangulo equilatero
+(2) Retangulo
+(3) Quadrado
+(4) Circulo
+(5) Piramide com base quadrangular
+(6) Cubo
+(7) Paralelepipedo
+(8) Esfera
+(0) Sair
+	
+}
---
+++ src/volume.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include "volume.h"
+
+float volumePiramide(float area_base, float altura){
+	return(equação);
+}
+float volumeCubo(float aresta){
+	return(equação);
+}
+float volumeParalelepipedo(){
+	return(equação);
+}
+float volumeEsfera(){
+	return(equação);
+}
