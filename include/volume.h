98456994f07d7f3859fbb4a0852c7313bfc4b8a9
Rodolpho Erick <rodolphoerick90@gmail.com>
2017-03-09 19:07:51 -0300

    Mudança de arquivos entre diretórios.

---
+++ include/volume.h
@@ -0,0 +1,9 @@
+#ifndef VOLUME_H
+#define VOLUME_H
+
+float volumePiramide(float area_base, float altura);
+float volumeCubo(float aresta);
+float volumeParalelepipedo(float aresta1, float aresta2, float aresta3);
+float volumeEsfera(float raio);
+
+#endif
