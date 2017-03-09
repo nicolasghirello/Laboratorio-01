2f07863902d91c2f71b98458a6828fd2173562c6
Rodolpho Erick <rodolphoerick90@gmail.com>
2017-03-09 19:04:14 -0300

    Funções para cálculo de área das figuras geométricas planas e espaciais.

---
+++ include/area.h
@@ -0,0 +1,22 @@
+#ifndef AREA_H
+#define AREA_H
+
+
+float areaTriangulo();
+
+float areaRetangulo();
+
+float areaQuadrado();
+
+float areaCirculo();
+
+float areaPiramide();
+
+float areaCubo();
+
+float areaParalelepipedo();
+
+float areaEsfera();
+
+
+#endif
